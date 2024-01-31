1a6fcc75054f3344898a7985642906ab57c718ed
dzming <dzm_work@163.com>
2024-01-31 12:33:58 +0000

    添加部分脚本

---
+++ proto_generate.sh
@@ -0,0 +1,17 @@
+#!/bin/bash
+
+protos=(
+    image_harmony
+    service_coordinator
+    target_detection
+    target_tracking
+    behavior_recognition
+)
+
+for proto in "${protos[@]}"
+do
+    mkdir -p ./generated/protos/$proto/
+    cp ./resources/protos/$proto.proto ./generated/protos/$proto/
+    python3 -m grpc_tools.protoc -I . --python_out=. --grpc_python_out=. ./generated/protos/$proto/$proto.proto
+    rm ./generated/protos/$proto/$proto.proto
+done
