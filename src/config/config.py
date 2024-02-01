773aefeb96c5dacda7474b9221ae7fa271878f3b
dzming <dzm_work@163.com>
2024-02-01 12:12:47 +0000

    权重的路径可在配置文件中指定

--- src/config/config.py
+++ src/config/config.py
@@ -16,8 +16,7 @@ class Config:
         self.service_name: str = ''
         self.service_port: str = ''
         self.service_tags: List[str] = list()
-        self.weights_map: Dict[str, WeightInfo] = dict()
-        self.weights_data_map: Dict[str, str] = dict()
+        self.weights_folder: str = ''
 
         #consul
         self.consul_ip: str
@@ -33,6 +32,7 @@ class Config:
         self.service_name = service_data.get('name', '')
         self.service_port = str(service_data.get('port', ''))
         self.service_tags = service_data.get('tags', [])
+        self.weights_folder = service_data.get('weights_folder', './weights')
         consul_data = config_data.get('consul', {})
         self.consul_ip = consul_data.get('ip', '')
         self.consul_port = consul_data.get('port', '')
