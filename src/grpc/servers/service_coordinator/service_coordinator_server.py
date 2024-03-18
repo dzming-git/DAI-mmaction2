8b61b8428e50d72175992a6430070685d32837ce
dzming <dzm_work@163.com>
2024-03-18 13:22:22 +0000

    适配任务管理器的管理逻辑的优化，并添加停止功能

--- src/grpc/servers/service_coordinator/service_coordinator_server.py
+++ src/grpc/servers/service_coordinator/service_coordinator_server.py
@@ -1,9 +1,5 @@
-from concurrent import futures
-import time
-import grpc
 import traceback
 from generated.protos.service_coordinator import service_coordinator_pb2, service_coordinator_pb2_grpc
-from typing import Dict
 from src.task_manager.task_manager import TaskManager, TaskInfo
 
 VALID_PRE_SERVICE = ['image harmony', 'target tracking']
@@ -17,31 +13,17 @@ class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer)
         try:
             task_manager = TaskManager()
             task_id = request.taskId
-            task_info: TaskInfo = None
 
-            if task_id in task_manager.tasks:
-                # task_id在task_manager.tasks存在
-                # 修改已有任务的参数
-                task_info = task_manager.tasks[task_id]
-            else:
-                # 新的task_id
-                task_info = TaskInfo(request.taskId)
-                task_manager.incomplete_tasks[task_id] = task_info
-                task_manager.tasks[task_id] = task_info
+            assert task_id in task_manager.tasks, f'task id {task_id} not init\n'
             assert request.preServiceName in VALID_PRE_SERVICE, 'invalid pre service\n'
             args = {}
             for arg in request.args:
                 args[arg.key] = arg.value
-            task_info.set_pre_service(
+            task_manager.tasks[task_id].set_pre_service(
                 pre_service_name=request.preServiceName,
                 pre_service_ip=request.preServiceIp,
                 pre_service_port=request.preServicePort,
                 args=args)
-            if task_id in task_manager.incomplete_tasks:
-                ok, msg = task_info.check()
-                if ok:
-                    task_manager.tasks_queue.put(task_info)
-                    task_manager.incomplete_tasks.pop(task_id)
 
         except Exception as e:
             response_code = 400
@@ -59,33 +41,18 @@ class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer)
         try:
             task_manager = TaskManager()
             task_id = request.taskId
-            task_info: TaskInfo = None
 
-            if task_id in task_manager.tasks:
-                # task_id在task_manager.tasks存在
-                # 修改已有任务的参数
-                task_info = task_manager.tasks[task_id]
-            else:
-                # 新的task id
-                task_info = TaskInfo(request.taskId)
-                task_manager.incomplete_tasks[task_id] = task_info
-                task_manager.tasks[task_id] = task_info
+            if task_id not in task_manager.tasks:
+                task_manager.tasks[task_id] = TaskInfo(request.taskId)
             
-            if task_id in task_manager.incomplete_tasks:
-                # task_id在task_manager.incomplete_tasks存在
-                # 修改已有任务的参数
-                task_info = task_manager.incomplete_tasks[task_id]
+            if task_id not in task_manager.tasks:
+                task_manager.tasks[task_id] = TaskInfo(request.taskId)
 
             args = {}
             for arg in request.args:
                 args[arg.key] = arg.value
-            task_info.set_cur_service(args)
-            
-            if task_id in task_manager.incomplete_tasks:
-                ok, msg = task_info.check()
-                if ok:
-                    task_manager.tasks_queue.put(task_info)
-                    task_manager.incomplete_tasks.pop(task_id)
+            task_manager.tasks[task_id].set_cur_service(args)
+
         except Exception as e:
             response_code = 400
             response_message += traceback.format_exc()
@@ -102,12 +69,28 @@ class ServiceCoordinatorServer(service_coordinator_pb2_grpc.CommunicateServicer)
             task_manager = TaskManager()
             task_id = request.taskId
             assert task_id in task_manager.tasks, 'ERROR: The task ID does not exist.\n'
-            tracker = task_manager.tasks[task_id].tracker
-        
+            task_start_ok, msg = task_manager.tasks[task_id].start()
+            assert task_start_ok, msg
         except Exception as e:
             response_code = 400
             response_message += traceback.format_exc()
         response = service_coordinator_pb2.StartResponse()
+        response.response.code = response_code
+        response.response.message = response_message
+        return response
+
+    def stop(self, request, context):
+        response_code = 200
+        response_message = ''
+        response = service_coordinator_pb2.StartResponse()
+        task_id = request.taskId
+        try:
+            task_manager = TaskManager()
+            task_manager.stop_task(task_id)
+        except Exception as e:
+            response.response.code = 400
+            response.response.message = traceback.format_exc()
+            return response
         response.response.code = response_code
         response.response.message = response_message
         return response
